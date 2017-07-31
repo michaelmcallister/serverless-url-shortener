e6061417959ee52b88ca38f474716ef997bba8be
Michael McAllister <git@skunkw0rks.io>
2017-07-31 19:35:05 +1000

    fixed broken test

--- util_test.go
+++ util_test.go
@@ -4,14 +4,14 @@ import "testing"
 
 func TestValidURL(t *testing.T) {
 	url := "https://www.google.com"
-	if !validURL(url) {
+	if !isValidProtocol(url) {
 		t.Errorf("URL '%s' was marked as an invalid url, expected: true", url)
 	}
 }
 
 func TestNonValidURL(t *testing.T) {
 	url := "ftp://www.google.com"
-	if validURL(url) {
+	if isValidProtocol(url) {
 		t.Errorf("URL '%s' was marked as valid, expected: false", url)
 	}
 }
